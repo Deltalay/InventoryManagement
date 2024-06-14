7d9efc2488bda0324d87967d08c141268ee75d56
Deltalay <paylayhok168@gmail.com>
2024-06-14 12:24:26 +0700

    Add database

---
+++ database.sql
@@ -0,0 +1,28 @@
+CREATE TABLE category (
+    id CHAR(36) PRIMARY KEY DEFAULT (UUID()),
+    name VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Create items table
+CREATE TABLE items (
+    id CHAR(36) PRIMARY KEY DEFAULT (UUID()),
+    category_id CHAR(36),
+    name VARCHAR(255) NOT NULL,
+    description VARCHAR(255),
+    price DOUBLE,
+    quantity INT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    expire_date TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY (category_id) REFERENCES category(id)
+);
+
+-- Create users table
+CREATE TABLE users (
+    id CHAR(36) PRIMARY KEY DEFAULT (UUID()),
+    username VARCHAR(255) NOT NULL UNIQUE,
+    role ENUM('admin', 'employee') NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
